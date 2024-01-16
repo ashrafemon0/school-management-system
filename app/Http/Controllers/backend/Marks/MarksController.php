8bf7ce82804554cac898d05a258012378ed2f94c
Ashraf Emon <emon.ashraf.017@gmail.com>
2024-01-16 15:10:37 +0600

    last git confilict change

--- app/Http/Controllers/backend/Marks/MarksController.php
+++ app/Http/Controllers/backend/Marks/MarksController.php
@@ -12,15 +12,10 @@ class MarksController extends Controller
 {
     public function StudentMarksAdd(){
 
-<<<<<<< HEAD
         $data['years'] = StudentYear::all();
         $data['classes'] = StudentClass::all();
         $data['exam_types'] = StudentExamModel::all();
-=======
-        $data['classes'] = StudentClass::all();
-        $data['years'] = StudentYear::all();
-        $data['exam'] = StudentExamModel::all();
->>>>>>> 001e104fcc67b0e7fd5ec022ebe34f7b2afc3527
+
 
         return view('admin.backend.Marks.marks_add',$data);
     }
